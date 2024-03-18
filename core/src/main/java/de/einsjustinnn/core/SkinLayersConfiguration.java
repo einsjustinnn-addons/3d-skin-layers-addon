d2c960079637f022e9ba1bd93127bc0dc66825be
EinsJustinnn <z0ck3r98hd@gmail.com>
2024-03-18 17:34:49 +0100

    removed ConfigName

--- core/src/main/java/de/einsjustinnn/core/SkinLayersConfiguration.java
+++ core/src/main/java/de/einsjustinnn/core/SkinLayersConfiguration.java
@@ -2,11 +2,9 @@ package de.einsjustinnn.core;
 
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
-import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.annotation.Exclude;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
-@ConfigName("settings")
 public class SkinLayersConfiguration extends AddonConfig {
 
   @Exclude
